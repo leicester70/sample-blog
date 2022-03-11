141460458d441574363ba2cec5800bee461d8669
leicester70 <lestercheong70@gmail.com>
2022-03-11 18:18:54 +0800

    changes

---
+++ src/create/create.js
@@ -0,0 +1,10 @@
+import '../App.css';
+
+const Create = () => {
+    return (
+        <p>This is create</p>
+        
+    );
+}
+ 
+export default Create;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Create from './create/create';
+
 import reportWebVitals from './reportWebVitals';
+// Import Routing
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <App /> }></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +28,6 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// Not used in this tutorial
+// reportWebVitals();
