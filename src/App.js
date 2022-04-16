b6020c07a5e52056718bc74c0f981533e4e0b3d1
leicester70 <lestercheong70@gmail.com>
2022-04-16 21:27:36 +0800

    scrollbar css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 
 // Import Styles
 import './App.css';
+import './codepenCSS/scrollbar.css'
 
 // Import Components
 import NavigationBar from './components/shared/navigationBar/navigationBar';
@@ -12,6 +13,9 @@ function App() {
   return (
     <div className="App">
       <NavigationBar></NavigationBar>
+      <div className="main-content-container">
+        
+      </div>
     </div>
 
   );
