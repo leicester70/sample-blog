c77f7f9b1549115e8788d790befa4f5548642e7d
Lester <lestercheong70@gmail.com>
2022-04-23 22:52:45 +0800

    time for some routing

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import App from './App';
 
 import reportWebVitals from './reportWebVitals';
 
+// Router
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
