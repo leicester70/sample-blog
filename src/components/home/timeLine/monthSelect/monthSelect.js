73522c886b37c8d35099ca479b6d1c4b1e1f696e
Lester <lestercheong70@gmail.com>
2022-04-23 21:11:46 +0800

    Gave month <p> abbreviation as keys. removed monthsArray from code and made into JSON

--- src/components/home/timeLine/monthSelect/monthSelect.js
+++ src/components/home/timeLine/monthSelect/monthSelect.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import monthsArray from "./monthsArray.json"
 
 class monthSelect extends Component {
 
@@ -6,66 +7,11 @@ class monthSelect extends Component {
     }
 
     render() {
-
-        const monthsArray = [
-            {
-                "abbreviation": "Jan",
-                "name": "January"
-            },
-            {
-                "abbreviation": "Feb",
-                "name": "February"
-            },
-            {
-                "abbreviation": "Mar",
-                "name": "March"
-            },
-            {
-                "abbreviation": "Apr",
-                "name": "April"
-            },
-            {
-                "abbreviation": "May",
-                "name": "May"
-            },
-            {
-                "abbreviation": "Jun",
-                "name": "June"
-            },
-            {
-                "abbreviation": "Jul",
-                "name": "July"
-            },
-            {
-                "abbreviation": "Aug",
-                "name": "August"
-            },
-            {
-                "abbreviation": "Sep",
-                "name": "September"
-            },
-            {
-                "abbreviation": "Oct",
-                "name": "October"
-            },
-            {
-                "abbreviation": "Nov",
-                "name": "November"
-            },
-            {
-                "abbreviation": "Dec",
-                "name": "December"
-            }
-        ]
-
-
         return (
             <div>
                 {
                     monthsArray.map((object =>
-                        <p key="">
-                            {object.name}
-                        </p>
+                        <p key={`${object.abbreviation}`}>{object.name}</p>
                     ))
                 }
             </div>
